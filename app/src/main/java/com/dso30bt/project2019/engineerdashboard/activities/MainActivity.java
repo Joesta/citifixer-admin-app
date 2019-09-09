3f77636084f199460da1d234a8e4aa8c83d5d342
Joesta <mogokong@moiponefleet.co.za>
2019-09-09 14:31:33 +0200

    Initial commit

---
+++ app/src/main/java/com/dso30bt/project2019/engineerdashboard/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dso30bt.project2019.engineerdashboard.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dso30bt.project2019.engineerdashboard.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
