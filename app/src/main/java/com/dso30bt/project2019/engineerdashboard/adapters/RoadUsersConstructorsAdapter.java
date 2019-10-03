313891b68e80978869cf2ad221d18c57f86381d4
Joesta <mogokong@moiponefleet.co.za>
2019-10-03 09:37:48 +0200

    modified models and added maps

---
+++ app/src/main/java/com/dso30bt/project2019/engineerdashboard/adapters/RoadUsersConstructorsAdapter.java
@@ -0,0 +1,7 @@
+package com.dso30bt.project2019.engineerdashboard.adapters;
+
+/**
+ * Created by Joesta on 2019/09/30.
+ */
+public class RoadUsersConstructorsAdapter {
+}
---
+++ app/src/main/java/com/dso30bt/project2019/engineerdashboard/utils/validators/CellNumberEntryValidator.java
@@ -0,0 +1,7 @@
+package com.dso30bt.project2019.engineerdashboard.utils.validators;
+
+/**
+ * Created by Joesta on 2019/10/01.
+ */
+public class CellNumberEntryValidator {
+}
